d1d6a69e81cf7dc9b6ff64c0524e974db3b3c22a
dai316 <179296165@qq.com>
2022-02-08 21:48:00 +0800

    1

--- code1.js
+++ code1.js
@@ -1,3 +1,18 @@
-let a=1.7;
-console.log(parseInt(a));
-console.log(parseInt(4/10))
+//两数之和
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let obj = {} //保存索引值
+    for(let i = 0;i<nums.length; i++){
+        let num =nums[i]
+        let n = target - num
+        if( num in obj ){
+            return [i,obj[num]]
+        }else {
+            obj[n]= i
+        }
+    }
+};
