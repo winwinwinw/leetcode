d58d290c85a8bfd80f17bc6818840f570a4c58ef
dai316 <179296165@qq.com>
2022-04-04 16:12:37 +0800

    2022

--- leetcode/editor/cn/[509]斐波那契数.js
+++ leetcode/editor/cn/[509]斐波那契数.js
@@ -53,10 +53,22 @@ var fib = function(n) {
     //     return n
     // }
     // return fib(n-1)+fib(n-2)
-    let dp = [0,1]
-    for (let i = 2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2]
+    // let dp = [0,1]
+    // for (let i = 2;i<=n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2]
+    // }
+    // return dp[n]
+    if(n<=1){
+        return n
     }
-    return dp[n]
+    let dp1 = 0
+    let dp2 = 1
+    let dp3
+    for (let i = 2;i<=n;i++){
+        dp3 = dp1 +dp2
+        dp1 = dp2
+        dp2 = dp3
+    }
+    return dp3
 };
 //leetcode submit region end(Prohibit modification and deletion)
