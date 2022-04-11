4458a98ace8f07c8d1dad8efc24eaa9b15cd67e8
dai316 <179296165@qq.com>
2022-04-11 21:39:29 +0800

    2022

---
+++ leetcode/editor/cn/[19]删除链表的倒数第 N 个结点.js
@@ -0,0 +1,70 @@
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1], n = 1
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [1,2], n = 1
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中结点的数目为 sz
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
+//
+//
+//
+//
+// 进阶：你能尝试使用一趟扫描实现吗？
+// Related Topics 链表 双指针 👍 1954 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode(null,head)
+    let fast = slow = dummy
+    while (n--){
+        fast = fast.next
+    }
+    while (fast.next!== null){
+        fast = fast.next
+        slow = slow.next
+    }
+    slow.next = slow.next.next
+    return dummy.next
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[206]反转链表.js
@@ -0,0 +1,71 @@
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1,2]
+//输出：[2,1]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = []
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目范围是 [0, 5000]
+// -5000 <= Node.val <= 5000
+//
+//
+//
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+//
+//
+// Related Topics 递归 链表 👍 2428 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head || !head.next){
+        return head
+    }
+    let perv = null
+    let cur = head
+    while (cur){
+        let next = cur.next
+        cur.next = perv
+        perv = cur
+        cur = next
+
+    }
+    return perv
+};
+//leetcode submit region end(Prohibit modification and deletion)
