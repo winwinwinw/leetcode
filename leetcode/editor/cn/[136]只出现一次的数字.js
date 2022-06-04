cbd901f57c0f01ae38aec9f917917878f181dfae
dai316 <179296165@qq.com>
2022-06-04 19:59:40 +0800

    136

---
+++ leetcode/editor/cn/[136]只出现一次的数字.js
@@ -0,0 +1,32 @@
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+//
+// 说明：
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+//
+// 示例 1:
+//
+// 输入: [2,2,1]
+//输出: 1
+//
+//
+// 示例 2:
+//
+// 输入: [4,1,2,1,2]
+//输出: 4
+// Related Topics 位运算 数组 👍 2434 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let result = 0
+    nums.forEach(num=>{
+        result ^= num
+    })
+    return result
+};
+//leetcode submit region end(Prohibit modification and deletion)
