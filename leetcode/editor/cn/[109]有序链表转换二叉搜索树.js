732164dcdbc61094160378fc6bd533204195062e
dai316 <179296165@qq.com>
2022-06-06 22:08:45 +0800

    109

---
+++ leetcode/editor/cn/[109]有序链表转换二叉搜索树.js
@@ -0,0 +1,100 @@
+//给定一个单链表的头节点 head ，其中的元素 按升序排序 ，将其转换为高度平衡的二叉搜索树。
+//
+// 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差不超过 1。
+//
+//
+//
+// 示例 1:
+//
+//
+//
+//
+//输入: head = [-10,-3,0,5,9]
+//输出: [0,-3,9,-10,null,5]
+//解释: 一个可能的答案是[0，-3,9，-10,null,5]，它表示所示的高度平衡的二叉搜索树。
+//
+//
+// 示例 2:
+//
+//
+//输入: head = []
+//输出: []
+//
+//
+//
+//
+// 提示:
+//
+//
+// head 中的节点数在[0, 2 * 10⁴] 范围内
+// -10⁵ <= Node.val <= 10⁵
+//
+// Related Topics 树 二叉搜索树 链表 分治 二叉树 👍 712 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+
+var sortedListToBST = function(head) {
+
+    function travese(head,tail){
+        if(head === tail){
+            return null
+        }
+        let slow = fast = head
+
+        while (fast !== tail && fast.next !== tail){
+            slow = slow.next
+            fast = fast.next.next
+        }
+        let root  = new TreeNode(slow.val)
+        root.left = travese(head,slow)
+        root.right = travese(slow.next,tail)
+        return root
+    }
+    return travese(head,null)
+
+};
+
+/////////////////11111111111111111111111
+// var sortedListToBST = function(head) {
+//     let arr = []
+//     let cur = head
+//     while (cur) {
+//         arr.push(cur.val)
+//         cur = cur.next
+//     }
+//
+// return sortedArrayToBST(arr)
+//
+// };
+// var sortedArrayToBST = function(nums) {
+//
+//     if(!nums.length){
+//         return null
+//     }
+//     const mid = Math.floor(nums.length/2)
+//     const root = new TreeNode(nums[mid])
+//     root.left = sortedArrayToBST(nums.slice(0,mid))
+//     root.right = sortedArrayToBST(nums.slice(mid+1))
+//     return root
+// };
+//leetcode submit region end(Prohibit modification and deletion)
